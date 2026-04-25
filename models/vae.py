836e885c4c015bf71548d00e289b89d435ebd506
macbook <macbook@Mac.lan>
2026-04-25 22:58:14 +0700

    add VAE loss function (BCE + KL)

--- models/vae.py
+++ models/vae.py
@@ -54,4 +54,12 @@ class VAE(nn.module):
         z = self.reparameterize(mu, logvar)
         x_recon = self.Decoder(z)
         return x_recon, mu, logvar
+def loss_function(x_recon, x, mu, logvar):
+    recon_loss = F.binary_cross_entropy(x_recon, x, reduction='sum')
+    
+    kl_loss = -0.5 * torch.sum(
+        1 + logvar - mu.pow(2) - logvar.exp()
+    )
+    
+    return recon_loss + kl_loss
 
